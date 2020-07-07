327f5064f59aca13fdc5059dab10be2ed33c4a6c
Vishal <vishaalbhardwaji8654@gmail.com>
2020-07-07 19:13:42 -0400

    Complaint submission

--- app/src/main/java/com/example/cegeproommatefinder/ComplaintActivity.java
+++ app/src/main/java/com/example/cegeproommatefinder/ComplaintActivity.java
@@ -2,11 +2,14 @@ package com.example.cegeproommatefinder;
 
 import android.os.Bundle;
 import android.*;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ComplaintActivity extends AppCompatActivity {
+public class ComplaintActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView mTextView;
 
@@ -15,8 +18,16 @@ public class ComplaintActivity extends AppCompatActivity {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
 
-
+            Button button = findViewById(R.id.SubmitButton);
+            button.setOnClickListener(this);
         // Enables Always-on
         //setAmbientEnabled();
+    }
+
+    @Override
+    public void onClick(View v) {
+        {
+            Toast.makeText(this,"Your Complaint Submitted!!!",Toast.LENGTH_SHORT).show();
+            }
     }
 }
