ec287d43b423cf54dc412d4537f2b8791af1582f
Gagandeep Singh Saini <gagan_singh18@protonmail.com>
2020-07-08 10:34:00 -0400

    Splash screen created

---
+++ app/src/main/java/com/example/cegeproommatefinder/Splashscreen.java
@@ -0,0 +1,28 @@
+package com.example.cegeproommatefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+    private final int DisplayTime = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(Splashscreen.this,MainActivity.class);
+                Splashscreen.this.startActivity(intent);
+                Splashscreen.this.finish();
+            }
+        }, DisplayTime);
+    }
+
+
+
+}
