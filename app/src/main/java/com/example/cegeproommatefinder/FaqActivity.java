bbca825c7aced05d81ada718ee2c3b1d07eef6a2
Vishal <vishaalbhardwaji8654@gmail.com>
2020-08-07 22:41:45 -0400

    Cegep Activity

--- app/src/main/java/com/example/cegeproommatefinder/FaqActivity.java
+++ app/src/main/java/com/example/cegeproommatefinder/FaqActivity.java
@@ -1,22 +1,27 @@
 package com.example.cegeproommatefinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FaqActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faq);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-
     }
+    public void AskMe(View view){
+        Intent intent=new Intent(this,ComplaintActivity.class);
+    }
+
+
 }
