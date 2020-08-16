0f90070e0a5dea1ca5e1af8a029d9cf661fd67dd
Vishal <vishaalbhardwaji8654@gmail.com>
2020-08-16 01:18:56 -0400

    complaint database

--- app/src/main/java/com/example/cegeproommatefinder/Complaint.java
+++ app/src/main/java/com/example/cegeproommatefinder/Complaint.java
@@ -1,7 +1,36 @@
 package com.example.cegeproommatefinder;
 
-public class Complaint
+import androidx.appcompat.app.AppCompatActivity;
 
-{
+public class Complaint {
+    private String message;
+    private Integer phone;
+    private String email;
 
+    public Complaint() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
