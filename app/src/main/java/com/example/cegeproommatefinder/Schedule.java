5a806c58cd743b42903e3a9ff61cf07ab9b74d2f
Manjinder Singh <manjinders2212@gmail.com>
2020-08-20 12:21:31 -0400

    initial

--- app/src/main/java/com/example/cegeproommatefinder/Schedule.java
+++ app/src/main/java/com/example/cegeproommatefinder/Schedule.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,10 +30,11 @@ import java.util.Calendar;
 
 import static java.util.Calendar.getInstance;
 
-public class Schedule extends AppCompatActivity implements OnDateSetListener {
+public class Schedule extends AppCompatActivity {
     private TextView datetext;
-
-EditText Name,Date,Time,Location,Email1,Email2,Email3;
+Button calen;
+EditText Name,Time,Location,Email1,Email2,Email3;
+TextView Date;
 
     DatabaseReference myRef;
     pojo pj;
@@ -54,6 +56,7 @@ EditText Name,Date,Time,Location,Email1,Email2,Email3;
         });
 
         Date=findViewById(R.id.show_date);
+        calen=findViewById(R.id.Calander_show);
         Time=findViewById(R.id.picktime);
         Location=findViewById(R.id.location);
 
@@ -62,6 +65,32 @@ EditText Name,Date,Time,Location,Email1,Email2,Email3;
         Email2=findViewById(R.id.email2);
             Email3=findViewById(R.id.email3);
           myRef=   FirebaseDatabase.getInstance().getReference("Meetings");
+          calen.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+                  Calendar calendar = getInstance();
+                  int YEAR = calendar.get(Calendar.YEAR);
+                  int MONTH = calendar.get(Calendar.MONTH);
+                  int DATE = calendar.get(Calendar.DATE);
+
+                  DatePickerDialog datePickerDialog = new DatePickerDialog(Schedule.this, new DatePickerDialog.OnDateSetListener() {
+                      @Override
+                      public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+
+                          Calendar calendar1 = getInstance();
+                          calendar1.set(Calendar.YEAR, year);
+                          calendar1.set(Calendar.MONTH, month);
+                          calendar1.set(Calendar.DATE, date);
+                          String dateText = DateFormat.format("dd MM yyyy", calendar1).toString();
+
+                          Date.setText(dateText);
+                      }
+                  }, YEAR, MONTH, DATE);
+
+                  datePickerDialog.show();
+              }
+          });
 
         }
        /* @RequiresApi(api = Build.VERSION_CODES.N)
@@ -107,9 +136,5 @@ sendmail();
 
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-     String date = "month/day/year:" + month +"/" + dayOfMonth + "/" + year;
-     datetext.setText(date);
-    }
+
 }
